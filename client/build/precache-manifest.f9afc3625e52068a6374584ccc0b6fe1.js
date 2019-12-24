f729b41b0fca1a457f4e9376e8d3b4e4f591809d
Povetkin Dmitriy <povdima19@gmail.com>
2019-12-24 21:03:39 +0200

    some updates

---
+++ client/build/precache-manifest.f9afc3625e52068a6374584ccc0b6fe1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e39d6624fc9d80e6aba900e6adccc52",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0b1f532a54553b15b0ca",
+    "url": "/static/css/main.f0d1140c.chunk.css"
+  },
+  {
+    "revision": "215647a4e672871526ee",
+    "url": "/static/js/2.bbe052c2.chunk.js"
+  },
+  {
+    "revision": "0b1f532a54553b15b0ca",
+    "url": "/static/js/main.d0d28e58.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
