441f65e653d877dd509e61385e146eed9435c424
Povetkin Dmitriy <povdima19@gmail.com>
2019-12-24 20:29:34 +0200

    update script

--- server.js
+++ server.js
@@ -15,8 +15,7 @@ mongoose
   .catch(err => console.log(err));
 
 app.use(middlewares);
-app.use('/api/analytics', analytics)
-
+app.use('/api/analytics', analytics);
 
 // Server static assets if in production
 if (process.env.NODE_ENV === 'production') {
@@ -25,6 +24,7 @@ if (process.env.NODE_ENV === 'production') {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
