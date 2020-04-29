da4713f500541b8b997e02c161949be7ea246575
Povetkin Dmitriy <povdima19@gmail.com>
2020-04-29 22:44:14 +0300

    test deploy

--- server.js
+++ server.js
@@ -1,25 +1,24 @@
-
 import express from 'express';
 import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
 import path from 'path';
 import middlewares from './middlewares';
-import analytics from './controllers/analytics'
-import { mongoURI } from './config/keys'
+import analytics from './controllers/analytics';
+import { mongoURI } from './config/keys';
 
 const app = express();
 
 mongoose
   .connect(mongoURI)
   .then(() => console.log('MongoDB connected'))
-  .catch(err => console.log(err));
+  .catch((err) => console.log(err));
 
 app.use(middlewares);
 app.use('/api/analytics', analytics);
 
 // Server static assets if in production
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));  
+  app.use(express.static('client/build'));
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
