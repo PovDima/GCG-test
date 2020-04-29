2483b965ab8c055bd9c384e58f2997ae6fb20b1c
Povetkin Dmitriy <povdima19@gmail.com>
2020-04-29 23:23:50 +0300

    fix  url

--- client/src/config.js
+++ client/src/config.js
@@ -1,3 +1,3 @@
 export default {
-  baseURL :'http://localhost:5000/api/analytics'
-}
+  baseURL: 'https://fast-scrubland-61054.herokuapp.com/api/analytics' //http://localhost:5000/api/analytics'
+};
