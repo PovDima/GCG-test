180f441cd0accb6e546ef8fed13c25977a7f45c1
Povetkin Dmitriy <povdima19@gmail.com>
2020-04-29 22:45:55 +0300

    test deploy

---
+++ client/build/precache-manifest.5b86676b900e7b14243b23e1bb4773a5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91f820b5380fb7256cdde1f0e4d7d7f3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "087bfb9c080c9eb981a9",
+    "url": "/static/css/main.f0d1140c.chunk.css"
+  },
+  {
+    "revision": "c442df4f35774ca12091",
+    "url": "/static/js/2.9e326491.chunk.js"
+  },
+  {
+    "revision": "087bfb9c080c9eb981a9",
+    "url": "/static/js/main.28bb71ae.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
